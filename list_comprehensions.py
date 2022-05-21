b2a72a17ddb9b51e961ea50b862123bd5384ec07
SURYANSH BHARDWAJ <suryanshbhardwaj46@gmail.com>
2022-05-21 23:44:18 +0530

    Update list_comprehensions.py

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -5,3 +5,5 @@ if __name__ == '__main__':
     n = int(input())
     ans = list([i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k)!=n)
     print(ans)
+
+    #List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list.
