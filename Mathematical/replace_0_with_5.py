628fa0611f5ea4801760dbaba2ec051abab1173e
SURYANSH BHARDWAJ <suryanshbhardwaj46@gmail.com>
2022-08-24 23:54:11 +0530

    Create replace_0_with_5.py

---
+++ Mathematical/replace_0_with_5.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convertFive(self,n):
+        #Code here
+        n=str(n)
+        ans=int(n.replace('0','5'))
+        return ans
+ 
+# n = integer 1005
+# output = 1555 integer
